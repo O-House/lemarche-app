33ada8e0a834675288f606eb512c36434d3c415f
cl4pper <cl4pper@gmail.com>
2022-04-12 11:49:52 +0100

    chore: setup env

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,51 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-	entry: path.resolve(__dirname, 'src', 'index.tsx'),
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'bundle.js',
-		publicPath: '/',
-	},
-	resolve: {
-		extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-		alias: {
-			'@styles': path.resolve(__dirname, 'src/styles'),
-			'@components': path.resolve(__dirname, 'src/components'),
+module.exports = (env) => {
+	return {
+		entry: path.resolve(__dirname, 'src', 'index.tsx'),
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: 'bundle.js',
+			publicPath: '/',
 		},
-	},
-	devServer: {
-		static: path.resolve(__dirname, 'build'),
-		historyApiFallback: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)?$/,
-				exclude: /node_modules/,
-				loader: require.resolve('babel-loader'),
+		resolve: {
+			extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+			alias: {
+				'@styles': path.resolve(__dirname, 'src/styles'),
+				'@components': path.resolve(__dirname, 'src/components'),
 			},
-			{
-				test: /\.(ts|tsx)?$/,
-				exclude: /node_modules/,
-				use: 'ts-loader',
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					{ loader: 'style-loader' },
-					{ loader: 'css-loader' },
-					{ loader: 'sass-loader', options: { additionalData: '@import "~@styles/global.scss";' } },
-				],
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: 'file-loader',
-			},
-		],
-	},
+		},
+		devServer: {
+			static: path.resolve(__dirname, 'build'),
+			historyApiFallback: true,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)?$/,
+					exclude: /node_modules/,
+					loader: require.resolve('babel-loader'),
+				},
+				{
+					test: /\.(ts|tsx)?$/,
+					exclude: /node_modules/,
+					use: 'ts-loader',
+				},
+				{
+					test: /\.scss$/,
+					use: [
+						{ loader: 'style-loader' },
+						{ loader: 'css-loader' },
+						{ loader: 'sass-loader', options: { additionalData: '@import "~@styles/global.scss";' } },
+					],
+				},
+				{
+					test: /\.(png|svg|jpg|gif)$/,
+					use: 'file-loader',
+				},
+			],
+		},
+		plugins: [new Dotenv({ path: `./env/.env${env.MODE ? `.${env.MODE}` : ''}` })],
+	}
 };
